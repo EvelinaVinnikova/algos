9e6d0f933366cb5710bae7405ccdc3dcb89b8c30
Evanderer <125723482+EvelinaVinnikova@users.noreply.github.com>
2025-04-24 04:50:18 +0000

    Reviewed

--- 1_minitask.py
+++ 1_minitask.py
@@ -1,18 +1,36 @@
-delim, delit = input().split()
-if int(delit) > int(delim):
-    print("The integer part of the division: 0")
-    print(f"The remainder of the division: {delim}")
-else:
-    n, m = len(delim), len(delit)
-    quotient = []
-    remainder = int(delim)
-    divisor = int(delit)
+def long_division(delimoe: int, delitel: int): # delimoe - n, delit - m
+    assert delitel > 0, "Error: Division by zero"
+    if delimoe < delitel:
+        return 0, delimoe
 
-    for i in range(n - m + 1): # n - m + 1 раз
-        prefix = remainder // (10 ** (n - m - i)) # 4 операции
-        digit = prefix // divisor #1 операция
-        quotient.append(str(digit))
-        remainder -= digit * divisor * (10 ** (n - m - i)) #6 операций
-# Всего около (n-m+1)*11 операций, множитель 11 не меняет порядок роста, поэтому О(11(n-m+1)) = O(n-m+1) = O(n-m)<O(n*m)
-    print(f"The integer part of the division: {''.join(quotient)}")
-    print(f"The remainder of the division: {remainder}")
+    whole_part = 0
+    remainder = 0
+
+    power = 1
+    while (delitel << power) <= delimoe: # n-m operations
+        power += 1
+
+    for i in reversed(range(power + 1)): #n-m operations
+        shifted = delitel << i
+        if shifted <= delimoe:
+            delimoe -= shifted
+            whole_part += (1 << i)
+        # total operations: 1 to 4
+
+    return whole_part, delimoe
+
+# In total k*(n-m) operations, if k is comparable to n or m then we have: kn- km = c1*n*m - c2*n*m = (c1-c2)*n*m
+# Worst case: 5 * (n-1) operations
+# Best case: (delitel and delimoe are equal) 3 operations
+
+q, r = long_division(200, 66)
+assert q == 3 and r == 2; "Error: wrong answer"
+
+q, r = long_division(123459532930350, 24591)
+assert q == 5020516974 and r == 22716; "Error: wrong answer"
+
+q, r = long_division(123459532930350, 123459302113484343)
+assert q == 0 and r == 123459532930350; "Error: wrong answer"
+
+q, r = long_division(20, 12)
+assert q == 1 and r == 8; "Error: wrong answer"
